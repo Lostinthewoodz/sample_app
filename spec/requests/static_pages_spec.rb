c110ec7fa4462fd63c2a71d34a68f02828409122
Woody Chung <wc9825@gmail.com>
2014-06-24 23:59:58 -0700

    v2

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -15,6 +15,18 @@ describe "Static pages" do
     end
   end
 
+  describe "Contact page" do
+    it "should have the content 'Contact'" do
+      visit '/static_pages/contact'
+      expect(page).to have_content('Contact')
+    end
+    it "should have the title 'Contact'" do
+      visit '/static_pages/home'
+      expect(page).to have_content("Ruby on Rails Tutorial Sample App | Contact")
+    end
+  end
+
+
   describe "Help page" do
 
     it "should have the content 'Help'" do
